42f534efe0bdbffeca38eb9940fb004699486e4d
corBharath <bharath@cortracker.com>
2022-12-06 20:59:46 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'aboutus', component:AboutUsComponent}
+  {path:'aboutus', component:AboutUsComponent},
+  {path:'contact', component:ContactsComponent}
 ];
 
 @NgModule({
