b837a71243f172c27e32ddb5620d8b6715530a8a
Magishetty Saikumar <108604044+saimagishetty@users.noreply.github.com>
2022-12-07 22:32:49 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ClientsComponent } from './pages/clients/clients.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'aboutus', component:AboutUsComponent},
-  {path:'contact', component:ContactsComponent}
+  {path:'contact', component:ContactsComponent},
+  {path:'products', component:ProductsComponent},
+  {path:'clinets', component:ClientsComponent},
+  
 ];
 
 @NgModule({
